e6189d2a7d98628a7f8f1e24955310b90b30fcb0
clong <colongxy@163.com>
2020-11-04 18:56:18 +0800

    调整 #clong

---
+++ src/views/runinfo/watersystem.vue
@@ -0,0 +1,393 @@
+<template>
+  <basic-container>
+    <avue-crud :option="option"
+               :table-loading="loading"
+               :data="data"
+               :page="page"
+               :permission="permissionList"
+               :before-open="beforeOpen"
+               v-model="form"
+               ref="crud"
+               @row-update="rowUpdate"
+               @row-save="rowSave"
+               @row-del="rowDel"
+               @search-change="searchChange"
+               @search-reset="searchReset"
+               @selection-change="selectionChange"
+               @current-change="currentChange"
+               @size-change="sizeChange"
+               @refresh-change="refreshChange"
+               @on-load="onLoad">
+      <template slot="menuLeft">
+        <el-button type="danger"
+                   size="small"
+                   icon="el-icon-delete"
+                   plain
+                   v-if="permission.watersystem_delete"
+                   @click="handleDelete">删 除
+        </el-button>
+      </template>
+      <template slot-scope="scope" slot="menu">
+        <el-button type="text" size="small" @click.stop="pushData(scope.row)" icon="el-icon-share">推送</el-button>
+      </template>
+    </avue-crud>
+  </basic-container>
+</template>
+
+<script>
+import {getList, getDetail, add, update, remove, pushData} from "@/api/runinfo/watersystem";
+import {mapGetters} from "vuex";
+import {pushBase} from "@/api/equipbase/equipbase";
+
+export default {
+  data() {
+    return {
+      form: {},
+      query: {},
+      loading: true,
+      page: {
+        pageSize: 10,
+        currentPage: 1,
+        total: 0
+      },
+      selectionList: [],
+      option: {
+        height: 'auto',
+        calcHeight: 30,
+        tip: false,
+        searchShow: true,
+        searchMenuSpan: 6,
+        border: true,
+        index: true,
+        viewBtn: true,
+        selection: true,
+        dialogClickModal: false,
+        column: [
+          {
+            label: "局码",
+            prop: "bureCode",
+            rules: [{
+              required: true,
+              message: "请输入局码",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "站码",
+            prop: "statCode",
+            rules: [{
+              required: true,
+              message: "请输入站码",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "设备id",
+            prop: "deviId",
+            rules: [{
+              required: true,
+              message: "请输入设备id",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管供水温度",
+            prop: "cwpSuTemp",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管供水温度",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管回水温度",
+            prop: "cwpReTemp",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管回水温度",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管供水流量",
+            prop: "cwpSuFlo",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管供水流量",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管回水流量",
+            prop: "cwpReFlo",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管回水流量",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管供水压力",
+            prop: "cwpSuPre",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管供水压力",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷却水总管回水压力",
+            prop: "cwpRePre",
+            rules: [{
+              required: true,
+              message: "请输入冷却水总管回水压力",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管供水温度",
+            prop: "fwpSuTemp",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管供水温度",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管回水温度",
+            prop: "fwpReTemp",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管回水温度",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管供水流量",
+            prop: "fwpSuFlo",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管供水流量",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管回水流量",
+            prop: "fwpReFlo",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管回水流量",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管供水压力",
+            prop: "fwpSuPre",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管供水压力",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "冷冻水总管回水压力",
+            prop: "fwpRePre",
+            rules: [{
+              required: true,
+              message: "请输入冷冻水总管回水压力",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "采集时间",
+            prop: "getTime",
+            rules: [{
+              message: "请输入采集时间",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "推送返回值",
+            prop: "pushType",
+            rules: [{
+              required: true,
+              message: "请输入推送返回值",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "推送返回值",
+            prop: "pushRes",
+            rules: [{
+              required: true,
+              message: "请输入推送返回值",
+              trigger: "blur"
+            }]
+          },
+          {
+            label: "推送返回值",
+            prop: "pushMsg",
+            rules: [{
+              required: true,
+              message: "请输入推送返回值",
+              trigger: "blur"
+            }]
+          },
+        ]
+      },
+      data: []
+    };
+  },
+  computed: {
+    ...mapGetters(["permission"]),
+    permissionList() {
+      return {
+        addBtn: this.vaildData(this.permission.watersystem_add, false),
+        viewBtn: this.vaildData(this.permission.watersystem_view, false),
+        delBtn: this.vaildData(this.permission.watersystem_delete, false),
+        editBtn: this.vaildData(this.permission.watersystem_edit, false)
+      };
+    },
+    ids() {
+      let ids = [];
+      this.selectionList.forEach(ele => {
+        ids.push(ele.id);
+      });
+      return ids.join(",");
+    }
+  },
+  methods: {
+    pushData(data) {
+      console.log('我在这儿呀', data);
+      this.$confirm("确定推送本条数据?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+          .then(() => {
+            pushData(data).then(() => {
+              this.onLoad(this.page);
+              this.$message({
+                type: "success",
+                message: "操作成功!"
+              });
+            }, error => {
+              console.log(error);
+            });
+          })
+    },
+    rowSave(row, loading, done) {
+      add(row).then(() => {
+        loading();
+        this.onLoad(this.page);
+        this.$message({
+          type: "success",
+          message: "操作成功!"
+        });
+      }, error => {
+        done();
+        console.log(error);
+      });
+    },
+    rowUpdate(row, index, loading, done) {
+      update(row).then(() => {
+        loading();
+        this.onLoad(this.page);
+        this.$message({
+          type: "success",
+          message: "操作成功!"
+        });
+      }, error => {
+        done();
+        console.log(error);
+      });
+    },
+    rowDel(row) {
+      this.$confirm("确定将选择数据删除?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+          .then(() => {
+            return remove(row.id);
+          })
+          .then(() => {
+            this.onLoad(this.page);
+            this.$message({
+              type: "success",
+              message: "操作成功!"
+            });
+          });
+    },
+    handleDelete() {
+      if (this.selectionList.length === 0) {
+        this.$message.warning("请选择至少一条数据");
+        return;
+      }
+      this.$confirm("确定将选择数据删除?", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+          .then(() => {
+            return remove(this.ids);
+          })
+          .then(() => {
+            this.onLoad(this.page);
+            this.$message({
+              type: "success",
+              message: "操作成功!"
+            });
+            this.$refs.crud.toggleSelection();
+          });
+    },
+    beforeOpen(done, type) {
+      if (["edit", "view"].includes(type)) {
+        getDetail(this.form.id).then(res => {
+          this.form = res.data.data;
+        });
+      }
+      done();
+    },
+    searchReset() {
+      this.query = {};
+      this.onLoad(this.page);
+    },
+    searchChange(params, done) {
+      this.query = params;
+      this.page.currentPage = 1;
+      this.onLoad(this.page, params);
+      done();
+    },
+    selectionChange(list) {
+      this.selectionList = list;
+    },
+    selectionClear() {
+      this.selectionList = [];
+      this.$refs.crud.toggleSelection();
+    },
+    currentChange(currentPage) {
+      this.page.currentPage = currentPage;
+    },
+    sizeChange(pageSize) {
+      this.page.pageSize = pageSize;
+    },
+    refreshChange() {
+      this.onLoad(this.page, this.query);
+    },
+    onLoad(page, params = {}) {
+      this.loading = true;
+      getList(page.currentPage, page.pageSize, Object.assign(params, this.query)).then(res => {
+        const data = res.data.data;
+        this.page.total = data.total;
+        this.data = data.records;
+        this.loading = false;
+        this.selectionClear();
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
